35251f85684db5f455969845b40aa585908beb89
Christian Anese <christian.anese@gmail.com>
2015-11-19 22:04:21 +0100

    Added bootstrap css and js to main page

--- src/main/java/org/scrum1/sese/view/page/MainPage.java
+++ src/main/java/org/scrum1/sese/view/page/MainPage.java
@@ -1,9 +1,19 @@
 package org.scrum1.sese.view.page;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.resource.CssResourceReference;
 
 public class MainPage extends WebPage {
 
 	private static final long serialVersionUID = 4809421004007068373L;
 
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		// TODO Auto-generated method stub
+		/*response.render(CssHeaderItem.forReference(new CssResourceReference(
+				MainPage.class, "bootstrap.css")));*/
+	}
+	
 }
