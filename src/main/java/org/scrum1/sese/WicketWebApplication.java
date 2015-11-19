5ef8244ea54ba01da6bb55fafe56aa613c65d0ba
Christian Anese <christian.anese@gmail.com>
2015-11-19 16:50:24 +0100

    Bootstrap setup

--- src/main/java/org/scrum1/sese/WicketWebApplication.java
+++ src/main/java/org/scrum1/sese/WicketWebApplication.java
@@ -4,12 +4,24 @@ import org.apache.wicket.Application;
 import org.apache.wicket.Page;
 import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.Url;
+import org.apache.wicket.request.resource.UrlResourceReference;
+import org.apache.wicket.resource.JQueryResourceReference;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.scrum1.sese.view.page.MainPage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.settings.BootstrapSettings;
+import de.agilecoders.wicket.core.settings.IBootstrapSettings;
+import de.agilecoders.wicket.core.settings.ThemeProvider;
+import de.agilecoders.wicket.less.BootstrapLess;
+import de.agilecoders.wicket.themes.markup.html.bootswatch.BootswatchTheme;
+import de.agilecoders.wicket.themes.markup.html.bootswatch.BootswatchThemeProvider;
 
 @EnableAutoConfiguration
 @ComponentScan
@@ -27,9 +39,35 @@ public class WicketWebApplication extends WebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WicketWebApplication.class, args);
 	}
-
+	
 	@Override
 	public RuntimeConfigurationType getConfigurationType() {
-		return super.getConfigurationType().DEPLOYMENT;
+		return RuntimeConfigurationType.DEVELOPMENT;
+	}
+	
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		super.init();
+		getComponentInstantiationListeners().add(new SpringComponentInjector(this));
+		configureBootstrap();
+        addResourceReplacement(JQueryResourceReference.get(),
+                new UrlResourceReference(
+                        Url.parse("http://code.jquery.com/jquery-1.11.0.min.js")));
+        
+        /*mountPage("account", UserAccountPage.class);
+        mountPage("fileUpload", FileUploadPage.class);*/
+	}
+
+	private void configureBootstrap() {
+		// TODO Auto-generated method stub
+		final IBootstrapSettings settings = new BootstrapSettings();
+        settings.useCdnResources(true);
+        
+        final ThemeProvider themeProvider = new BootswatchThemeProvider(BootswatchTheme.Spacelab);
+        settings.setThemeProvider(themeProvider);
+        
+        Bootstrap.install(this, settings);
+        BootstrapLess.install(this);
 	}
 }
--- src/main/java/org/scrum1/sese/view/page/MainPage.java
+++ src/main/java/org/scrum1/sese/view/page/MainPage.java
@@ -1,6 +1,11 @@
 package org.scrum1.sese.view.page;
 
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
+
+import de.agilecoders.wicket.core.Bootstrap;
+
 
 public class MainPage extends WebPage {
 
