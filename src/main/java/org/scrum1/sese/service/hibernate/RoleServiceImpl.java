3888d8761bf5c735b2f58c730bf6db41128d0255
Tomschi <e0601280@student.tuwien.ac.at>
2015-11-22 11:04:30 +0100

    #60
    create service impl example class.

---
+++ src/main/java/org/scrum1/sese/service/hibernate/RoleServiceImpl.java
@@ -0,0 +1,20 @@
+package org.scrum1.sese.service.hibernate;
+
+import org.scrum1.sese.repository.RoleRepository;
+import org.scrum1.sese.service.RoleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+	private final static Logger logger = LoggerFactory.getLogger(RoleServiceImpl.class);
+
+	@Autowired private MessageSource messageSource;
+	@Autowired private RoleRepository roleRepository;
+
+
+}
