52058b44ca43282a2bbe6b1a1dfe1c298513c3f6
Tomschi <e0601280@student.tuwien.ac.at>
2015-11-24 15:10:34 +0100

    create RoomServiceImpl and add findAll method.

--- src/main/java/org/scrum1/sese/service/RoomService.java
+++ src/main/java/org/scrum1/sese/service/RoomService.java
@@ -1,5 +1,11 @@
 package org.scrum1.sese.service;
 
+import java.util.List;
+
+import org.scrum1.sese.dbo.Room;
+
 public interface RoomService {
+
+	public List<Room> findAll();
 
 }
---
+++ src/main/java/org/scrum1/sese/service/hibernate/RoomServiceImpl.java
@@ -0,0 +1,29 @@
+package org.scrum1.sese.service.hibernate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.scrum1.sese.dbo.Room;
+import org.scrum1.sese.dbo.hibernate.RoomImpl;
+import org.scrum1.sese.repository.RoomRepository;
+import org.scrum1.sese.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RoomServiceImpl implements RoomService {
+
+	@Autowired private RoomRepository roomRepository;
+
+	private List<Room> toList(Iterable<RoomImpl> iterable) {
+		List<Room> rooms = new ArrayList<>();
+		for(Room room : iterable) {
+			rooms.add(room);
+		}
+		return rooms;
+	}
+
+	@Override
+	public List<Room> findAll() {
+		return toList(roomRepository.findAll());
+	}
+
+}
