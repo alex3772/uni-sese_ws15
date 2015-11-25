6f9d841a25b8b382781bf1fc23ae2adc8cee50bc
Tomschi <e0601280@student.tuwien.ac.at>
2015-11-25 20:36:41 +0100

    #4
    refactore room page.

--- src/main/java/org/scrum1/sese/view/page/BasePage.java
+++ src/main/java/org/scrum1/sese/view/page/BasePage.java
@@ -1,22 +1,21 @@
 package org.scrum1.sese.view.page;
 
-import java.util.Date;
-
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.scrum1.sese.view.page.panels.NavigationPanel;
 
 public class BasePage extends WebPage{
 
+	private static final long serialVersionUID = 4340442722408654508L;
+
 	public BasePage() {
 		this(null);
 	}
-	
-	BasePage(IModel model) {
+
+	public BasePage(IModel<?> model) {
 		super(model);
 		this.add(new NavigationPanel("mainNav"));
 	}
-	
-	
+
+
 }
--- src/main/java/org/scrum1/sese/view/page/RoomListPage.java
+++ src/main/java/org/scrum1/sese/view/page/RoomListPage.java
@@ -1,22 +1,21 @@
 package org.scrum1.sese.view.page;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.scrum1.sese.dbo.Room;
-import org.scrum1.sese.dbo.hibernate.RoomImpl;
-import org.scrum1.sese.repository.RoomRepository;
 import org.scrum1.sese.service.RoomService;
-import org.scrum1.sese.service.hibernate.RoomServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 
-public class RoomListPage extends BasePage{
-	
+public class RoomListPage extends BasePage {
+
+	private static final long serialVersionUID = -8144829479277470172L;
+
 	@SpringBean
 	private RoomService roomService;
 
@@ -27,57 +26,37 @@ public class RoomListPage extends BasePage{
 		} else {
 			System.out.println("No username set");
 		}
-		
-		addSampleRooms();
-	}
-	
-	public RoomListPage() {
-		this.roomService = new RoomServiceImpl();
-		
-		System.out.println("SERVICE" + this.roomService);
-		
-		
-		try {
-			System.out.println("Inside try");
-			List<Room> testList = this.roomService.findAll();
-			if(testList.isEmpty()) {
-				System.out.println("List is empty");
-			} else {
-				System.out.println("Not empty");
-			}
-		} catch (Exception e) {
-			System.out.println("ERROR " + e);
-		}
-		
-		addSampleRooms();
 	}
 
-	private void addSampleRooms() {
-		List<RoomImpl> rooms = new LinkedList<RoomImpl>();
-		
-		RoomImpl r1 = new RoomImpl();
-		r1.setName("test");
-		r1.setMaxPersons(10);
-		
-		RoomImpl r2 = new RoomImpl();
-		r2.setName("test 2");
-		r2.setMaxPersons(2);
-		
-		rooms.add(r1);
-		rooms.add(r2);
-		
-		System.out.println("Created rooms");
-		
-		ListView roomsListView = new ListView("rooms_list", rooms) {
+	public RoomListPage() {
+		this.add(getRoomListView(getRoomListModel()));
+	}
+
+	private ListView<Room> getRoomListView(IModel<List<Room>> roomListModel) {
+		return new ListView<Room>("rooms_list", roomListModel) {
+
+			private static final long serialVersionUID = -155003224331087777L;
+
 			@Override
-			protected void populateItem(ListItem item) {
-				RoomImpl r = (RoomImpl) item.getModelObject();
-				item.add(new Label("name", r.getName()));
-				item.add(new Label("maxPersons", r.getMaxPersons()));
+			protected void populateItem(ListItem<Room> item) {
+				Room room = item.getModelObject();
+				item.add(new Label("name", room.getName()));
+				item.add(new Label("maxPersons", room.getMaxPersons()));
 			}
 		};
-		
-		this.add(roomsListView);		
 	}
-	
+
+	private IModel<List<Room>> getRoomListModel() {
+		return new LoadableDetachableModel<List<Room>>() {
+
+			private static final long serialVersionUID = -1830981661911150014L;
+
+			@Override
+			protected List<Room> load() {
+				return roomService.findAll();
+			}
+		};
+	}
+
+
 }
