fcd5290b9c18981511682413bde308ae20d4635b
Tomschi <e0601280@student.tuwien.ac.at>
2015-11-26 20:07:56 +0100

    #8 #9 #17
    add service methods for crud and search operations.

--- src/main/java/org/scrum1/sese/service/ReservationService.java
+++ src/main/java/org/scrum1/sese/service/ReservationService.java
@@ -1,5 +1,26 @@
 package org.scrum1.sese.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.scrum1.sese.dbo.Customer;
+import org.scrum1.sese.dbo.Reservation;
+import org.scrum1.sese.dbo.Room;
+
 public interface ReservationService {
+
+	public Reservation save(Reservation reservation);
+
+	public void delete(Reservation reservation);
+
+	public List<Reservation> findAll();
+
+	public List<Reservation> findAll(Customer customer);
+
+	public List<Reservation> findAll(Room room);
+
+	public List<Reservation> findAll(LocalDate checkin, LocalDate checkout);
+
+	public List<Reservation> findAll(Customer customer, Room room, LocalDate checkin, LocalDate checkout);
 
 }
--- src/main/java/org/scrum1/sese/service/hibernate/ReservationServiceImpl.java
+++ src/main/java/org/scrum1/sese/service/hibernate/ReservationServiceImpl.java
@@ -1,30 +1,119 @@
 package org.scrum1.sese.service.hibernate;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-import org.apache.commons.collections.IteratorUtils;
 import org.scrum1.sese.dbo.Customer;
 import org.scrum1.sese.dbo.Reservation;
+import org.scrum1.sese.dbo.Room;
+import org.scrum1.sese.dbo.hibernate.CustomerImpl;
 import org.scrum1.sese.dbo.hibernate.ReservationImpl;
+import org.scrum1.sese.dbo.hibernate.RoomImpl;
 import org.scrum1.sese.repository.ReservationRepository;
 import org.scrum1.sese.repository.specification.ReservationSpecification;
 import org.scrum1.sese.service.ReservationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.transaction.annotation.Transactional;
 
 public class ReservationServiceImpl implements ReservationService {
 
+	private final static Logger logger = LoggerFactory.getLogger(ReservationImpl.class);
+
+	@Autowired private MessageSource messageSource;
 	@Autowired private ReservationRepository reservationRepository;
 
+	private List<Reservation> toList(Iterable<ReservationImpl> iterable) {
+		List<Reservation> reservations = new ArrayList<>();
+		for(Reservation reservation : iterable) {
+			reservations.add(reservation);
+		}
+		return reservations;
+	}
+
+	private IllegalArgumentException generateIllegalArgumentException(String messageKey) {
+		String message = messageSource.getMessage(messageKey, null, Locale.getDefault());
+		IllegalArgumentException exception = new IllegalArgumentException(message);
+		logger.error(message, exception);
+		return exception;
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public Reservation save(Reservation reservation) {
+		if(reservation instanceof ReservationImpl) {
+			return reservationRepository.save((ReservationImpl) reservation);
+		} else {
+			throw generateIllegalArgumentException("error.reservationimpl.type");
+		}
+	}
+
+	@Override
+	@Transactional(readOnly = false)
+	public void delete(Reservation reservation) {
+		if(reservation instanceof ReservationImpl) {
+			reservationRepository.delete((ReservationImpl) reservation);
+		} else {
+			throw generateIllegalArgumentException("error.reservationimpl.type");
+		}
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Reservation> findAll() {
+		return toList(reservationRepository.findAll());
+	}
+
+	@Override
 	@Transactional(readOnly = true)
 	public List<Reservation> findAll(Customer customer) {
-		Specification<ReservationImpl> reservaionSpecification = new ReservationSpecification(customer);
+		if(customer instanceof CustomerImpl) {
+			return toList(reservationRepository.findByCustomer(customer));
+		} else {
+			throw generateIllegalArgumentException("error.customerimpl.type");
+		}
+	}
 
-		@SuppressWarnings("unchecked")
-		List<Reservation> reservations = IteratorUtils.toList(reservationRepository.findAll(reservaionSpecification).iterator());
+	@Override
+	@Transactional(readOnly = true)
+	public List<Reservation> findAll(Room room) {
+		if(room instanceof RoomImpl) {
+			return toList(reservationRepository.findByRoom(room));
+		} else {
+			throw generateIllegalArgumentException("error.roomimpl.type");
+		}
+	}
 
-		return reservations;
+	@Override
+	@Transactional(readOnly = true)
+	public List<Reservation> findAll(LocalDate checkin, LocalDate checkout) {
+		if(checkin != null && checkout != null) {
+			return toList(reservationRepository.findByCheckinAndCheckout(checkin, checkout));
+		} else if(checkin != null) {
+			return toList(reservationRepository.findByCheckin(checkin));
+		} else {
+			return toList(reservationRepository.findByCheckout(checkout));
+		}
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Reservation> findAll(Customer customer, Room room, LocalDate checkin, LocalDate checkout) {
+		if(!(customer instanceof CustomerImpl)) {
+			throw generateIllegalArgumentException("error.customerimpl.type");
+		}
+
+		if(!(room instanceof RoomImpl)) {
+			throw generateIllegalArgumentException("error.roomimpl.type");
+		}
+
+		Specification<ReservationImpl> reservaionSpecification = new ReservationSpecification(customer, room, checkin, checkout);
+		return toList(reservationRepository.findAll(reservaionSpecification));
 	}
 
 }
