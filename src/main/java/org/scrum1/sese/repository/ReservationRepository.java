cfc506322a21fb10333943b4c6ed958178e54864
Tomschi <e0601280@student.tuwien.ac.at>
2015-11-26 20:07:08 +0100

    #8 #9 #17
    add concrete query methods.

--- src/main/java/org/scrum1/sese/repository/ReservationRepository.java
+++ src/main/java/org/scrum1/sese/repository/ReservationRepository.java
@@ -1,5 +1,10 @@
 package org.scrum1.sese.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.scrum1.sese.dbo.Customer;
+import org.scrum1.sese.dbo.Room;
 import org.scrum1.sese.dbo.hibernate.ReservationImpl;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
@@ -8,5 +13,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReservationRepository extends CrudRepository<ReservationImpl, Long>,
 	JpaSpecificationExecutor<ReservationImpl> {
+
+	public List<ReservationImpl> findByCustomer(Customer customer);
+
+	public List<ReservationImpl> findByRoom(Room room);
+
+	public List<ReservationImpl> findByCheckin(LocalDate checkin);
+
+	public List<ReservationImpl> findByCheckout(LocalDate checkou);
+
+	public List<ReservationImpl> findByCheckinAndCheckout(LocalDate checkin, LocalDate checkout);
+
+	public List<ReservationImpl> findByCustomerAndRoom(Customer customer, Room room);
 
 }
